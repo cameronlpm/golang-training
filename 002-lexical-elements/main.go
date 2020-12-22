9e667f449c07eccde8990c8f5af989e098945bb3
Cameron Moore <cam@cameronlpmoore.com>
2020-12-22 15:34:49 -0800

    short declaration operator and var

--- 002-lexical-elements/main.go
+++ 002-lexical-elements/main.go
@@ -2,13 +2,22 @@ package main
 
 import "fmt"
 
+// global
+var g = 3
+
 func main() {
+	// short declaration operator :=
+	// declare a variable and assign a value at the same time
 	x := 42
 	fmt.Println(x)
-	x = 99
+	x = 99 // x already declared
 	fmt.Println(x)
 	y := x + 1
 	fmt.Println(y)
 	z := "Bond, James"
 	fmt.Println(z)
+
+	// var keyword 
+	// can be used to create global variables outside of a function body 
+	fmt.Println("Global identifier g has a value of", g)
 }
